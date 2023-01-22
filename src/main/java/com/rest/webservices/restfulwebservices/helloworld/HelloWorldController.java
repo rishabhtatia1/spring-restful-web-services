9f948a146c0af19baaba8da1b9d5a9ee9ff6d447
rishabhtatia1 <rishabh.tatia@codeastu.com>
2023-01-22 12:11:58 +0530

    added initial setup and user endpoint

---
+++ src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,2 @@
+package com.rest.webservices.restfulwebservices.helloworld;public class HelloWorldController {
+}
---
+++ src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -0,0 +1,2 @@
+package com.rest.webservices.restfulwebservices.user;public class User {
+}
