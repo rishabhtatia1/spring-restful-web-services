9320dcaeaef39301f9d855e58663dbd283377082
rishabhtatia1 <rishabh.tatia@codeastu.com>
2023-01-22 14:03:18 +0530

    added custom exeption handling to return errros

--- src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,2 +1,42 @@
-package com.rest.webservices.restfulwebservices.user;public class UserResource {
+package com.rest.webservices.restfulwebservices.user;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+    private UserDaoService service;
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+    @GetMapping("/users")
+    public List<User> retieveAllUsers() {
+        return  service.findAll();
+    }
+    @GetMapping("/users/{id}")
+    public User retieveById(@PathVariable Integer id) {
+        User user = service.findOne(id);
+        if (user == null) {
+            throw new UserNotFoundException("id: " + id);
+        }
+        return  user;
+    }
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+    User savedUser = service.save(user);
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                   .path("/{id}")
+                   .buildAndExpand(savedUser.getId())
+                   .toUri();
+    return ResponseEntity.created(location).build();
+    }
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        service.deleteById(id);
+    }
 }
