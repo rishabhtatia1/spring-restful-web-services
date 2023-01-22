eaf5cd89e8c24fb43bec5d2a420eafa6dc1c941e
rishabhtatia1 <rishabh.tatia@codeastu.com>
2023-01-22 18:20:08 +0530

    connected with sql database and added spring security

--- src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -1,25 +1,50 @@
 package com.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
+import org.springframework.context.annotation.Primary;
 
 import java.time.LocalDate;
+import java.util.List;
 
+@Entity(name= "user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
     @Size(min=2, message = "Name should have atleast 2 characters")
-    @JsonProperty("user_name")
+    //@JsonProperty("user_name")
     private String name;
     @Past(message = "Birth Date should be in the past")
-    @JsonProperty("birth_date")
+    //@JsonProperty("birth_date")
     private LocalDate birthDate;
-
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Post> posts;
     public User(Integer id, String name, LocalDate birthDate) {
         super();
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    public User() {
+
     }
 
     public Integer getId() {
